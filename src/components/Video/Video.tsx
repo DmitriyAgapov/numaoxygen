4749d23bda173a47148103e3a9934b924a2ca6b9
DmitriyAgapov <93589129+DmitriyAgapov@users.noreply.github.com>
2024-11-20 07:20:08 +0500

    fix

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,9 +1,7 @@
 'use client'
-import { CSSProperties,  useCallback, useEffect, useRef, useState } from "react";
-
-import { useIsVisible } from "@/utils/hooks";
-import ReactPlayer, { ReactPlayerProps } from "react-player";
+import React, { CSSProperties, useEffect,  useState } from "react";
 import Image from "next/image";
+import ReactPlayer from "react-player";
 
 type VideoComponentProps = {
 	src: string;
@@ -17,60 +15,66 @@ export const VideoComponent = ({
 	style,
 	alt,
 }: VideoComponentProps) => {
-
-	const { isVisible, targetRef } = useIsVisible(
-		{
-			root: null,
-			rootMargin: "200px",
-			threshold: 0.1,
-		},
-		false,
-	);
-
-	const videoRef = useRef<HTMLVideoElement>(null);
-
-	const startVideoOnMouseMove = useCallback(async () => {
-		try {
-			if(videoRef && videoRef.current)  {
-				await videoRef.current.play();
-			}
-		} catch (e) {
-			console.log(e)
-			// do nothing
-		}
-	}, []);
-
-	const stopVideoOnMove = useCallback(() => {
-		try {
-			if(videoRef && videoRef.current) {
-				videoRef.current.pause();
-			}
-		} catch (e) {
-			console.log(e)
-			// do nothing
-		}
-	}, []);
-
+	//
+	// const { isVisible, targetRef } = useIsVisible(
+	// 	{
+	// 		root: null,
+	// 		rootMargin: "200px",
+	// 		threshold: 0.1,
+	// 	},
+	// 	false,
+	// );
+	//
+	// const videoRef = useRef<ReactPlayer | undefined>(null);
+	//
+	// const startVideoOnMouseMove = useCallback(async () => {
+	// 	try {
+	// 		if(videoRef && videoRef.current)  {
+	// 			await videoRef.current.play();
+	// 		}
+	// 	} catch (e) {
+	// 		console.log(e)
+	// 		// do nothing
+	// 	}
+	// }, []);
+	//
+	// const stopVideoOnMove = useCallback(() => {
+	// 	try {
+	// 		if(videoRef && videoRef.current) {
+	// 			videoRef;
+	// 		}
+	// 	} catch (e) {
+	// 		console.log(e)
+	// 		// do nothing
+	// 	}
+	// }, []);
+	//
+	// useEffect(() => {
+	// 	if (isVisible) {
+	// 		startVideoOnMouseMove();
+	// 	} else {
+	// 		stopVideoOnMove();
+	// 	}
+	// }, [isVisible, startVideoOnMouseMove, stopVideoOnMove]);
+	const [ player, setPlayer ] = useState<React.ReactNode | null>(null);
 	useEffect(() => {
-		if (isVisible) {
-			startVideoOnMouseMove();
-		} else {
-			stopVideoOnMove();
-		}
-	}, [isVisible, startVideoOnMouseMove, stopVideoOnMove]);
-	const [player, setPlayer] = useState<React.ReactNode |null>(null);
-	useEffect(() => {
-		setPlayer(() => <ReactPlayer  width={"100%"} height={"100%"}  style={{
-			objectFit: "contain",
-			aspectRatio: "16/9",
-			display: "block",
-			width: "100%",
-			height: "100%",
-			...style,
-		}}  url={src} fallback={poster ? <Image src={poster as string} alt={""}/> : <></> } />)
-	}, [src, poster, style]);
+		setPlayer(() => <ReactPlayer width={"100%"}
+			height={"100%"}
+			style={{
+				objectFit: "contain",
+				aspectRatio: "16/9",
+				display: "block",
+				width: "100%",
+				height: "100%",
+				...style,
+			}}
+			url={src}
+			fallback={poster ? <Image src={poster as string}
+				alt={""}/> : <></>}/>)
+	}, [ src, poster, style ]);
+
 	return (
-		<span ref={targetRef as any} title={alt}
+		<span title={alt}
 			style={{
 				position: "relative",
 				minHeight: "50px",
