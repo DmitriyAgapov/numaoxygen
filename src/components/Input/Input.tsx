9b41039588b8f2fd72219e7b43c3a529be6ee8b3
DmitriyAgapov <93589129+DmitriyAgapov@users.noreply.github.com>
2024-11-26 01:25:13 +0500

    modal & Numa => Uma

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-// import styles from './Input.module.scss';
 
 const Input = ({label, ...props}: {label: string} & React.InputHTMLAttributes<any>) => {
 	return (
 		<label className={"block"} htmlFor={props.name}>
 			{label && <span className={'text-sm'}>{label}</span>}
-			<input className={"mt-1 block w-full"} {...props}/>
+			<input className={"mt-1 block w-full"} id={props.name} {...props}/>
 		</label>
 	);
 };
